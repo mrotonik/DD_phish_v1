f69481a3c2fc3355cb16081f157818ea7a4d0fac
mrotonik <71843904+mrotonik@users.noreply.github.com>
2022-06-11 13:28:57 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,21 @@
+import phishing_detection
+from flask import Flask
+from flask import (
+    render_template, request
+)
+app = Flask(__name__)
+
+
+
+
+@app.route('/', methods = ['GET', 'POST'])
+def hello():
+	if request.method == 'POST':
+		try:
+			username = request.form.get('DDK')
+			return render_template("getInput.html",items=phishing_detection.getResult(username))
+		except:
+			pass
+	return  render_template("getInput.html")
+if __name__ == '__main__':
+    app.run(debug=True)
